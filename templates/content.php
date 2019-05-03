79af7e26a64459df3497132ca71390560d2d265f
forrestkirby <18228336+forrestkirby@users.noreply.github.com>
2019-05-03 12:34:22 +0200

    Delete content.php

--- templates/content.php
+++
@@ -1,6 +0,0 @@
-<div>
-
-	<div><?= $props['field_content'] ?></div>
-	<progress value="$props['field_stop']" max="<?= $props['field_max'] ?>"><?= $props['field_stop'] ?>/<?= $props['field_max'] ?></progress>
-
-</div>
