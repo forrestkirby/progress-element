ef180b4312399206e9d0adcf174d57c4099a5787
forrestkirby <18228336+forrestkirby@users.noreply.github.com>
2019-05-03 12:31:40 +0200

    Add files via upload

---
+++ progress/templates/content.php
@@ -0,0 +1,6 @@
+<div>
+
+	<div><?= $props['field_content'] ?></div>
+	<progress value="$props['field_stop']" max="<?= $props['field_max'] ?>"><?= $props['field_stop'] ?>/<?= $props['field_max'] ?></progress>
+
+</div>
