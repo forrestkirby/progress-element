54b26b8e34a1f66cf8492fea3c42e401e9b2d67a
forrestkirby <18228336+forrestkirby@users.noreply.github.com>
2019-05-05 15:59:11 +0200

    Update content.php

--- progress/templates/content.php
+++ progress/templates/content.php
@@ -1,6 +1,6 @@
 <div>
 
-	<div><?= $props['field_content'] ?></div>
-	<progress value="$props['field_stop']" max="<?= $props['field_max'] ?>"><?= $props['field_stop'] ?>/<?= $props['field_max'] ?></progress>
+	<div><?= $props['content'] ?></div>
+	<progress value="$props['stop']" max="<?= $props['max'] ?>"><?= $props['stop'] ?>/<?= $props['max'] ?></progress>
 
 </div>
