df972f95ff5751f26cecda7046185ca29ce485a7
forrestkirby <18228336+forrestkirby@users.noreply.github.com>
2019-05-07 09:27:35 +0200

    fixed echo of $props['stop']

--- progress/templates/content.php
+++ progress/templates/content.php
@@ -1,6 +1,6 @@
 <div>
 
 	<div><?= $props['content'] ?></div>
-	<progress value="$props['stop']" max="<?= $props['max'] ?>"><?= $props['stop'] ?>/<?= $props['max'] ?></progress>
+	<progress value="<?= $props['stop'] ?>" max="<?= $props['max'] ?>"><?= $props['stop'] ?>/<?= $props['max'] ?></progress>
 
 </div>
